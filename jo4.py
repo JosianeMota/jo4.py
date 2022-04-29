a8ae164739058367d4d39f5ab089ae11ef747849
JosianeMota <102743914+JosianeMota@users.noreply.github.com>
2022-04-29 14:03:26 -0300

    Add files via upload

---
+++ jo4.py
@@ -0,0 +1,8 @@
+PrimeiroNome = input("digite seu primeiro nome:")
+dia = input("dia do nascimento:")
+mês = input("mês do nascimento:")
+ano = input(" ano de nascimento:")
+
+mensagem = "{} nasceu no dia: {}/{}/{}"
+
+print(mensagem.format(PrimeiroNome, dia, mês, ano))
